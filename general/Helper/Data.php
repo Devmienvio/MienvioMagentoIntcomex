400164e9601898843037e2500aa708bfaec8d25f
Miguel <miguelmalagon@outlook.com>
2019-03-14 17:00:41 -0600

    init files

---
+++ general/Helper/Data.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace mienvio\general\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+
+    const XML_PATH_HELLOWORLD = 'configs/';
+
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field, ScopeInterface::SCOPE_STORE, $storeId
+        );
+    }
+
+    public function getGeneralConfig($code, $storeId = null)
+    {
+
+        return $this->getConfigValue(self::XML_PATH_HELLOWORLD .'general/'. $code, $storeId);
+    }
+
+}
