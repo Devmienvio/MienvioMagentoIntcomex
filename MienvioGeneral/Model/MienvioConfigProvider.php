b693486801f35a1b074fff7c1317127f412fff46
Miguel <miguelmalagon@outlook.com>
2019-03-20 16:43:13 -0600

    add store info on checkout

--- MienvioGeneral/Model/MienvioConfigProvider.php
+++ MienvioGeneral/Model/MienvioConfigProvider.php
@@ -5,21 +5,33 @@ namespace MienvioMagento\MienvioGeneral\Model;
 
 use MienvioMagento\MienvioGeneral\Helper\Data as Helper;
 use \Magento\Checkout\Model\ConfigProviderInterface;
+use \Magento\Store\Model\Information as Info;
 
 class MienvioConfigProvider implements ConfigProviderInterface
 {
 
     public function __construct(
-        Helper $helperData
+        Helper $helperData,
+        Info $storeInfo
     )
     {
-        $this->mienvioHelper = $helperData;
+        $this->_mienvioHelper = $helperData;
+        $this->_storeInfo = $storeInfo;
     }
+
+    public function getPhoneNumber()
+    {
+        return $this->_storeInfo->getStoreInformationObject()->getPhone();
+}
 
     public function getConfig()
     {
+        $storeInfo = [];
+        $storeInfo['phone'] = $this->getPhoneNumber();
+
         $config = [];
-        $config['mienvioApiKey'] = $this->mienvioHelper->getMienvioApi();
+        $config['mienvioApiKey'] = $this->_mienvioHelper->getMienvioApi();
+        $config['storeInfo'] = $storeInfo;
         return $config;
     }
 }
