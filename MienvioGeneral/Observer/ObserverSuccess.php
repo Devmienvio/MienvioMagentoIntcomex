858f6e6175610155c0c97a5930d1f88f26b341d3
Jorge Luis González Sánchez <jorgegonzac@gmail.com>
2019-04-04 09:07:32 -0600

    handle level1 field at observer

--- MienvioGeneral/Observer/ObserverSuccess.php
+++ MienvioGeneral/Observer/ObserverSuccess.php
@@ -83,7 +83,8 @@ class ObserverSuccess implements ObserverInterface
                 $this->_mienvioHelper->getOriginStreet2(),
                 $this->_mienvioHelper->getOriginZipCode(),
                 "ventas@mienvio.mx",
-                "5551814040"
+                "5551814040",
+                $shippingAddress->getCountryId()
             );
 
             $toStreet2 = empty($shippingAddress->getStreetLine(2)) ? $shippingAddress->getStreetLine(1) : $shippingAddress->getStreetLine(2);
@@ -95,7 +96,8 @@ class ObserverSuccess implements ObserverInterface
                 $shippingAddress->getPostcode(),
                 $customermail,
                 $customerPhone,
-                $shippingAddress->getStreetLine(3)
+                $shippingAddress->getStreetLine(3),
+                $shippingAddress->getCountryId()
             );
 
 
@@ -338,21 +340,30 @@ class ObserverSuccess implements ObserverInterface
      * @param  string $email
      * @param  string $phone
      * @param  string $reference
+     * @param  string $countryId
      * @return string
      */
-    private function createAddressDataStr($name, $street, $street2, $zipcode, $email, $phone, $reference = '.')
+    private function createAddressDataStr($name, $street, $street2, $zipcode, $email, $phone, $reference = '.', $countryId)
     {
         $street = substr($street, 0, 35);
         $street2 = substr($street2, 0, 35);
         $name = substr($name, 0, 80);
         $phone = substr($phone, 0, 20);
+        $zipcodeFieldName = 'zipcode';
+        $country = 'MX';
+
+        if ($destCountryId === 'PE') {
+            $zipcodeFieldName = 'level_1';
+            $country = 'PE';
+        }
 
         return '{
             "object_type": "PURCHASE",
             "name": "'. $name . '",
             "street": "'. $street . '",
             "street2": "'. $street2 . '",
-            "zipcode": '. $zipcode . ',
+            "' . $zipcodeFieldName .'": '. $zipcode . ',
+            "country": ' . $country . ',
             "email": "'. $email .'",
             "phone": "'. $phone .'",
             "reference": "'. $reference .'"
