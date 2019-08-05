933d302b61c146ff1195a9e54a2ac01d0c2a1e78
Jorge Luis González Sánchez <jorgegonzac@gmail.com>
2019-08-05 18:40:33 -0500

    try without the -

--- MienvioGeneral/Model/Carrier/Mienviorates.php
+++ MienvioGeneral/Model/Carrier/Mienviorates.php
@@ -195,7 +195,7 @@ class Mienviorates extends AbstractCarrier implements CarrierInterface
 
             foreach ($rates as $rate) {
                 $this->_logger->debug('rate_id');
-                $methodId = strval($rate['id']) . '-' . $rate['servicelevel'] . '-' . $rate['courier'];
+                $methodId = strval($rate['id']) .  $rate['servicelevel'] . $rate['courier'];
                 $this->_logger->debug((string)$methodId);
                 $this->_logger->debug(strval($rate['id']));
 
@@ -213,6 +213,7 @@ class Mienviorates extends AbstractCarrier implements CarrierInterface
             $this->_logger->debug("Rates Exception");
             $this->_logger->debug($e);
         }
+
         return $rateResponse;
     }
 
